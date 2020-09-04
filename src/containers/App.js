563b025028891096be46220aaf80cd010396b8c1
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-04 06:44:31 -0500

    Added routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router";
+
 
 // Components
+import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
 import SideBar from '../components/SideBar';
+import ReservationList from './ReservationList';
+import RoomList from './RoomList';
+import Reservation from './Reservation';
 
 function App() {
   return (
-    <div className="App">
-      <SideBar />
-    </div>
+    <Router>
+      <div className="App">
+        <SideBar />
+        <Switch>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/reservations">
+            <ReservationList />
+          </Route>
+          <Route path="/reservation/:id">
+            <Reservation />
+          </Route>
+          <Route path="/">
+            <RoomList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
