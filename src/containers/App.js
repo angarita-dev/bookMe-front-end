a8fe8d3beed2739babe77ae15a0667ef59a8244e
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-04 06:31:15 -0500

    Added side bar component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// Components
+import SideBar from '../components/SideBar';
+
 function App() {
   return (
     <div className="App">
+      <SideBar />
     </div>
   );
 }
