bd576b5e0ce2b8f92e15fc664db3cb6d81409932
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-06 14:50:13 -0500

    Added sidebar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function SideBar(props) {
-  const { loggedUser } = props;
+// Actions
+import { logOut } from '../redux/actions/index';
 
+function SideBar({ loggedUser, logOut }) {
   const reservationsLink = loggedUser
     ? <NavLink to="/reservations">Reservations</NavLink>
     : null;
+
+  const accountLinks = loggedUser
+    ? <NavLink to="/" onClick={logOut}>Log out</NavLink>
+    : (
+      <div>
+        <NavLink
+          to="/sign-in"
+        >
+          Sign In
+        </NavLink>
+        <NavLink
+          to="/sign-up"
+        >
+          Sign Up
+        </NavLink>
+      </div>
+    );
 
   return (
     <div className="sidebar">
@@ -20,26 +39,20 @@ export default function SideBar(props) {
         >
           Rooms
         </NavLink>
-        <NavLink
-          to="/sign-in"
-        >
-          Sign In
-        </NavLink>
-        <NavLink
-          to="/sign-up"
-        >
-          Sign Up
-        </NavLink>
         { reservationsLink }
+        { accountLinks }
       </div>
     </div>
   );
 }
 
-SideBar.defaultProps = {
-  loggedUser: false,
+SideBar.propTypes = {
+  loggedUser: PropTypes.bool.isRequired,
+  logOut: PropTypes.func.isRequired,
 };
 
-SideBar.propTypes = {
-  loggedUser: PropTypes.bool,
-};
+const mapStateToProps = state => ({
+  loggedUser: state.user.token !== undefined && state.user.token.length > 0,
+});
+
+export default connect(mapStateToProps, { logOut })(SideBar);
