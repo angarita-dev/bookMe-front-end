591761b738498621ed6e16a8f252a93ca3d60595
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-09-06 14:50:27 -0500

    Added room list component

--- src/containers/RoomList.js
+++ src/containers/RoomList.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // Components
 import Room from '../components/Room';
 
-export default function RoomList() {
+const RoomList = ({ rooms }) => {
+  const roomElements = rooms.map(room => (
+    <Room
+      key={room.id}
+      id={room.id}
+      title={room.name}
+      capacity={room.capacity}
+      privateRoom={room.private}
+      amenities={room.amenities}
+      imgUrl={room.img_url}
+    />
+  ));
+
   return (
     <div className="room-list">
-      <Room displayReservation />
+      { roomElements }
     </div>
   );
-}
+};
+
+RoomList.propTypes = {
+  rooms: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => ({
+  rooms: state.rooms,
+});
+
+export default connect(mapStateToProps)(RoomList);
